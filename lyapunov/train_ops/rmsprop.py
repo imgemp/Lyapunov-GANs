7b33aebfa4fe7a77110f1ea9d60b489c7289368a
Ian Gemp <imgemp@gmail.com>
2018-09-22 14:10:31 -0400

    Switching back to epsilon inside square root for more reliable gradients
    gradient of sqrt(x^2) near zero is infinite due to chain rule decomposition: 1/2*z^(-1/2)*2x where z = x^2.
    https://github.com/pytorch/pytorch/issues/2421

--- lyapunov/train_ops/rmsprop.py
+++ lyapunov/train_ops/rmsprop.py
@@ -17,7 +17,7 @@ class RMSProp(Map):
         g_a_map = [(1-self.alpha)/self.m.params['gen_learning_rate']*(a-g**2.) for a, g in zip(aux_g, g_error_grad)]
 
         # 2. Compute main map
-        d_map = [g/(torch.sqrt(self.alpha*a+(1-self.alpha)*g**2.)+self.epsilon) for a, g in zip(aux_d, d_error_grad)]
-        g_map = [g/(torch.sqrt(self.alpha*a+(1-self.alpha)*g**2.)+self.epsilon) for a, g in zip(aux_g, g_error_grad)]
+        d_map = [g/(torch.sqrt(self.alpha*a+(1-self.alpha)*g**2.+self.epsilon)) for a, g in zip(aux_d, d_error_grad)]
+        g_map = [g/(torch.sqrt(self.alpha*a+(1-self.alpha)*g**2.+self.epsilon)) for a, g in zip(aux_g, g_error_grad)]
 
         return [aux_d, aux_g, d_map, g_map, d_a_map, g_a_map, V, norm_d, norm_g]
